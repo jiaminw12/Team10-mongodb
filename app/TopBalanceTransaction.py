04c19d87c90d52a25b6f603d9f351b67d773bac8
Jia Min <jiaminw@ymail.com>
2017-10-25 17:12:41 +0800

    Inserted TopBalanceTransaction

--- app/TopBalanceTransaction.py
+++ app/TopBalanceTransaction.py
@@ -18,32 +18,14 @@ class TopBalanceTransaction(object):
 
 	def __init__(self, session ):
 		self.session = session
-		self.initPreparedStmts()
-	
-	def initPreparedStmts(self):
-		self.select_stmt = self.session.prepare("SELECT c_w_id, c_d_id, c_first, c_middle, c_last, c_balance FROM top_balance LIMIT 10");
-		self.select_w_name = self.session.prepare("SELECT w_name FROM warehouse WHERE w_id = ?");
-		self.select_d_name = self.session.prepare("SELECT d_name FROM district WHERE d_w_id = ? AND d_id = ?");
 
 	def process(self):
-		# (TOPBALANCE) SELECT C_FIRST, C_MIDDLE, C_LAST, C_BALANCE, W_NAME, D_NAME FROM TOP_BALANCE LIMIT 10
-		
-		result_count = self.session.execute(self.select_stmt)
-		
-		result_list = []
-		for row in result_count:
-			c = Customer(row[0], row[1], row[2], row[3], row[4], row[5])
-			result_list.append(c)
-		
-		result_list.sort(key=lambda x: x.c_balance, reverse=True)
-		
-		for row in result_list:
+		customerCollection = self.session.customer
+		cbalanceCollection = customerCollection.find().sort({("c_balance", -1)}).limit(10)
 			
-			result_w_name = self.session.execute(self.select_w_name, [row.c_w_id])
-			result_d_name = self.session.execute(self.select_d_name, [row.c_w_id, row.c_d_id])
-
-			print "C_FIRST: %s,  C_MIDDLE: %s,  C_LAST: %s"%(row.c_first, row.c_middle, row.c_last)
-			print "C_BALANCE: %f"%(row.c_balance)
-			print "W_NAME: %s"%(result_w_name[0])
-			print "D_NAME: %s"%(result_d_name[0])
+		for result in cbalanceCollection:
+			print "C_FIRST: %s,  C_MIDDLE: %s,  C_LAST: %s"%(result["c_first"], result["c_middle"], result["c_last"])
+			print "C_BALANCE: %f"%(result["c_balance"])
+			print "W_NAME: %s"%(result["w_name"])
+			print "D_NAME: %s"%(result["d_name"])
 			print "\n"
