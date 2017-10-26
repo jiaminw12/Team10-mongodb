d1554185b7201b9e7589abcf591e0ff0b3ab4d1c
Jia Min <jiaminw@ymail.com>
2017-10-27 00:28:02 +0800

    Updated TopBalanceTransaction

--- app/TopBalanceTransaction.py
+++ app/TopBalanceTransaction.py
@@ -5,15 +5,6 @@ import pymongo
 from decimal import *
 from operator import itemgetter, attrgetter, methodcaller
 
-class Customer(object):
-    def __init__(self, c_w_id, c_d_id, c_first, c_middle, c_last, c_balance):
-		self.c_w_id = c_w_id
-		self.c_d_id = c_d_id
-		self.c_first = c_first
-		self.c_middle = c_middle
-		self.c_last = c_last
-		self.c_balance = c_balance
-
 class TopBalanceTransaction(object):
 
 	def __init__(self, session ):
@@ -21,7 +12,7 @@ class TopBalanceTransaction(object):
 
 	def process(self):
 		customerCollection = self.session.customer
-		cbalanceCollection = customerCollection.find().sort({("c_balance", -1)}).limit(10)
+		cbalanceCollection = list(customerCollection.find({"w_name":{"$exists":"true"}}).sort([("c_balance", -1)]).limit(10))
 			
 		for result in cbalanceCollection:
 			print "C_FIRST: %s,  C_MIDDLE: %s,  C_LAST: %s"%(result["c_first"], result["c_middle"], result["c_last"])
