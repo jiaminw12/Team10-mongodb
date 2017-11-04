059352b4d4dbb1ba3b02cab9d4e1d0133e711c46
Chen Fang Li <chenfang26@gmail.com>
2017-11-04 14:55:07 +0800

    update payment and delivery transaction

--- app/DeliveryTransaction.py
+++ app/DeliveryTransaction.py
@@ -6,65 +6,31 @@ import decimal
 from datetime import datetime
 
 class DeliveryTransaction(object):
-	def __init__(self, session, w_id, carrier_id):
-		self.session = session
-		self.w_id = int(w_id)
-		self.carrier_id = int(carrier_id)
-		self.initPreparedStmts()
+        def __init__(self, session, w_id, carrier_id):
+                self.session = session
+                self.w_id = int(w_id)
+                self.carrier_id = int(carrier_id)
 
-	def initPreparedStmts(self):
-		self.min_order_number_query = self.session.prepare(
-				"SELECT o_id, o_c_id FROM order_by_asc "
-				"WHERE o_w_id = ? AND o_d_id = ? AND o_carrier_id = 0 "
-				"LIMIT 1 ALLOW FILTERING")
-		self.select_all_ols_by_order = self.session.prepare(
-				"SELECT ol_number, ol_amount FROM orderline "
-				"WHERE ol_w_id = ? AND ol_d_id = ? AND ol_o_id = ?")
-		self.update_order_by_desc_query = self.session.prepare(
-				"UPDATE order_by_desc SET o_carrier_id = ?"
-				"WHERE o_w_id = ? AND o_d_id = ? AND o_id = ?")
-		self.update_order_by_asc_query = self.session.prepare(
-                                "UPDATE order_by_asc SET o_carrier_id = ?"
-                                "WHERE o_w_id = ? AND o_d_id = ? AND o_id = ?")
-		self.update_order_line_query = self.session.prepare(
-				"UPDATE orderline SET ol_delivery_d = ?"
-				"WHERE ol_w_id = ? AND ol_d_id = ? AND ol_o_id = ? AND ol_number = ?")
-		self.select_payment_by_customer_query = self.session.prepare(
-				"SELECT c_balance, c_delivery_cnt FROM payment_by_customer "
-				"WHERE c_w_id = ? AND c_d_id = ? AND c_id = ?")
-		self.update_payment_by_customer_query = self.session.prepare(
-				"UPDATE payment_by_customer SET c_balance = ?, c_delivery_cnt= ? "
-				"WHERE c_w_id = ? AND c_d_id = ? AND c_id = ?")
+        def process(self):
+                for district_no in range(1, 11):
+                        self.get_next_order_and_customer(district_no)
 
-	def process(self):
-		for district_no in range(1, 11):
-			self.get_next_order_and_customer(district_no)
-
-	def get_next_order_and_customer(self, district_no):
-		min_order = self.session.execute(self.min_order_number_query, (self.w_id, district_no))
-		
-		if min_order:
-			self.X = min_order[0][0]
-			self.C = min_order[0][1]
-			ol_amount = 0.00
-			for ol_in_order in self.session.execute(self.select_all_ols_by_order, (self.w_id, district_no, self.X)):
-				ol_amount = ol_amount + float(ol_in_order[1])
-				self.update_order_by_desc_asc(district_no)
-				self.update_order_line(district_no, ol_in_order[0])
-			self.ol_amount = ol_amount
-			self.update_payment_by_customer(district_no)
-
-	def update_order_by_desc_asc(self, district_no):
-		self.session.execute(self.update_order_by_desc_query, (self.carrier_id, self.w_id, district_no, self.X))
-		self.session.execute(self.update_order_by_asc_query, (self.carrier_id, self.w_id, district_no, self.X))
-
-	def update_order_line(self, district_no, ol_number):
-		self.session.execute(self.update_order_line_query, (datetime.now(), self.w_id, district_no, self.X, ol_number))
-
-	def update_payment_by_customer(self, district_no):
-		customer_attr = self.session.execute(self.select_payment_by_customer_query, (self.w_id, district_no, self.C))
-		self.c_balance = customer_attr[0][0] + decimal.Decimal(self.ol_amount)
-		self.c_delivery_cnt = customer_attr[0][1] + 1
-		self.session.execute(self.update_payment_by_customer_query, (self.c_balance, self.c_delivery_cnt, self.w_id, district_no, self.C))
-
+        def get_next_order_and_customer(self, district_no):
+                customerCollection = self.session.customer
+                orderCollection = self.session.order
+                #TODO o_carrier_id null ? has not solved
+                min_order = list(orderCollection.find({"o_w_id":self.w_id, "o_d_id":district_no, "o_carrier_id": 1}).sort([("o_id", 1)]).limit(1))
+                if min_order:
+                        order_id = min_order[0]["o_id"]
+                        customer = min_order[0]["o_c_id"]
+                        # Update carrier id
+                        orderCollection.update({"o_w_id":self.w_id, "o_d_id":district_no, "o_id": order_id}, {"$set": {"o_carrier_id":self.carrier_id}})
+                        ol_amount = 0.00
+                        for ol_in_order in min_order[0]["o_line"]:
+                                ol_amount = ol_amount + float(ol_in_order["ol_amount"])
+                                orderCollection.update({"o_w_id":self.w_id, "o_d_id":district_no, "o_id": order_id, "o_line.ol_number":ol_in_order["ol_number"]}, {"$set": {"ol_delivery_d":datetime.now()}})
+                        selected_customer = customerCollection.find_one({"c_w_id":self.w_id,"c_d_id":district_no,"c_id":customer})
+                        c_balance_to_update = selected_customer["c_balance"] + float(ol_amount)
+                        c_delivery_cnt_to_update = selected_customer["c_delivery_cnt"] + 1
+                        customerCollection.update({"c_w_id":self.w_id, "c_d_id":district_no,"c_id": customer},{"$set":{"c_balance":float(c_balance_to_update), "c_delivery_cnt": c_delivery_cnt_to_update}})
 
--- app/PaymentTransaction.py
+++ app/PaymentTransaction.py
@@ -4,45 +4,29 @@ import pymongo
 import decimal
 
 class PaymentTransaction(object):
-	
-	def __init__(self, session, c_w_id, c_d_id, c_id, payment):
-		self.session = session
-		self.c_w_id = int(c_w_id)
-		self.c_d_id = int(c_d_id)
-		self.c_id = int(c_id)
-		self.payment = decimal.Decimal(payment)
-		self.initPreparedStmts()
-	
-	def initPreparedStmts(self):
-		self.update_warehouse_query = self.session.prepare("UPDATE warehouse SET w_ytd = ? WHERE w_id = ?")
-		self.update_district_query = self.session.prepare("UPDATE district SET d_ytd = ? WHERE d_w_id = ? AND d_id = ?")
-		self.select_payment_by_customer_query = self.session.prepare("SELECT c_balance, c_ytd_payment, c_payment_cnt FROM payment_by_customer WHERE c_w_id = ? AND c_d_id = ? AND c_id = ?")
-		self.update_payment_by_customer_query = self.session.prepare("UPDATE payment_by_customer SET c_balance = ?, c_ytd_payment = ?,c_payment_cnt = ? WHERE c_w_id = ? AND c_d_id = ? AND c_id = ?")
-		self.output_query = self.session.prepare("SELECT * FROM payment_by_customer WHERE c_w_id = ? AND c_d_id = ? AND c_id = ?")
 
-	def process(self):
-		self.update_warehouse()
-		self.update_district()
-		self.update_customer()
-		self.retrieve_output()
+        def __init__(self, session, c_w_id, c_d_id, c_id, payment):
+                self.session = session
+                self.c_w_id = int(c_w_id)
+                self.c_d_id = int(c_d_id)
+                self.c_id = int(c_id)
+                self.payment = payment
 
-	def update_warehouse(self):
-		self.session.execute(self.update_warehouse_query, (self.payment, self.c_w_id))
-
-	def update_district(self):
-		self.session.execute(self.update_district_query, (self.payment, self.c_w_id, self.c_d_id))
-	
-	def update_customer(self):
-		customer_attr = self.session.execute(self.select_payment_by_customer_query, (self.c_w_id, self.c_d_id, self.c_id))
-		if customer_attr:
-			self.c_balance_to_update = customer_attr[0][0] - self.payment
-			self.c_ytd_payment_to_update = customer_attr[0][1] + float(self.payment)
-			self.c_payment_cnt_to_update = customer_attr[0][2] + 1
-			self.session.execute(self.update_payment_by_customer_query,(self.c_balance_to_update, self.c_ytd_payment_to_update, self.c_payment_cnt_to_update, self.c_w_id, self.c_d_id, self.c_id))
-
-	def retrieve_output(self):
-		output_attr = self.session.execute(self.output_query, (self.c_w_id, self.c_d_id, self.c_id))
-		print ("customer identifier: %d, %d, %d" % (self.c_w_id, self.c_d_id, self.c_id))
-		print ("warehouse address: %s, %s, %s, %s, %s" % (output_attr[0].w_address.street_1, output_attr[0].w_address.street_2, output_attr[0].w_address.city, output_attr[0].w_address.state, output_attr[0].w_address.zip))
-		print ("district address: %s, %s, %s, %s, %s" % (output_attr[0].d_address.street_1, output_attr[0].d_address.street_2, output_attr[0].d_address.city, output_attr[0].d_address.state, output_attr[0].d_address.zip))
-		print ("payment amount: %.2f\n" % self.payment)
+        def process(self):
+                warehouseCollection = self.session.warehouse_district
+                customerCollection = self.session.customer
+                # update warehouse District
+                warehouseDistrictRow = [{"$unwind":"$district"},{"$match":{"w_id":self.c_w_id, "district.d_id":self.c_d_id}}]
+                warehouseCollection.update({"w_id":self.c_w_id, "district.d_id":self.c_d_id}, {"$set":{"w_ytd": float(self.payment), "district.$.d_ytd":float(self.payment)}})
+                selected_warehouse_district = list(warehouseCollection.aggregate(warehouseDistrictRow))[0]
+                # update customer
+                selected_customer = customerCollection.find_one({"c_w_id":self.c_w_id,"c_d_id":self.c_d_id,"c_id":self.c_id})
+                c_balance_to_update = selected_customer["c_balance"] - float(self.payment)
+                c_ytd_payment_to_update = selected_customer["c_ytd_payment"] + float(self.payment)
+                c_payment_cnt_to_update = selected_customer["c_payment_cnt"] + 1
+                customerCollection.update({"c_w_id":self.c_w_id, "c_d_id":self.c_d_id,"c_id": self.c_id},{"$set":{"c_balance":float(c_balance_to_update),"c_ytd_payment":float(c_ytd_payment_to_update),"c_payment_cnt": c_payment_cnt_to_update}})
+                #retrieve output
+                print ("customer identifier: %d, %d, %d name: %s, %s, %s address %s, %s, %s, %s, %s phone %s since %s credit %s credit_lim %s c_discount %s c_balance %s" % (self.c_w_id, self.c_d_id, self.c_id, selected_customer["c_first"], selected_customer["c_middle"], selected_customer["c_last"], selected_customer["c_address"]["c_street_1"],selected_customer["c_address"]["c_street_2"], selected_customer["c_address"]["c_city"], selected_customer["c_address"]["c_state"], selected_customer["c_address"]["c_zip"], selected_customer["c_phone"], selected_customer["c_since"], selected_customer["c_credit"],selected_customer["c_credit_lim"], selected_customer["c_discount"], c_balance_to_update))
+                print ("warehouse address: %s, %s, %s, %s, %s" % (selected_warehouse_district["w_address"]["w_street_1"], selected_warehouse_district["w_address"]["w_street_2"], selected_warehouse_district["w_address"]["w_city"], selected_warehouse_district["w_address"]["w_state"], selected_warehouse_district["w_address"]["w_zip"]))
+                print ("district address: %s, %s, %s, %s, %s" % (selected_warehouse_district["district"]["d_address"]["d_street_1"], selected_warehouse_district["district"]["d_address"]["d_street_2"], selected_warehouse_district["district"]["d_address"]["d_city"], selected_warehouse_district["district"]["d_address"]["d_state"], selected_warehouse_district["district"]["d_address"]["d_zip"]))
+                print ("payment amount: %.2f\n" % float(self.payment))
