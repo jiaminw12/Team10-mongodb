fd6d0fc1c7d856c47172e58065e113d53f4fbaf2
Jia Min <jiaminw@ymail.com>
2017-11-08 17:02:45 +0800

    Fixed Stock-Level

--- app/StockLevelTransaction.py
+++ app/StockLevelTransaction.py
@@ -8,15 +8,11 @@ class StockLevelTransaction(object):
 
 	def __init__(self, session, w_id, d_id, stockThreshold, numOfLastOrder):
 		self.session = session
-	#	self.consistencyLevel = consistencyLevel
 		self.w_id = w_id
 		self.d_id = d_id
 		self.stockThreshold = stockThreshold
 		self.numOfLastOrder = numOfLastOrder
-		
 	
-	
-		
 	def process(self):
 		
 		# prepare statement
@@ -26,19 +22,16 @@ class StockLevelTransaction(object):
 
 		# get set of items from the last L orders
 		startOrderId = resultDNextOId - int(self.numOfLastOrder)
-		lastOrderListCollection = self.session.order.find({"o_w_id":int(self.w_id), "o_d_id": int(self.d_id), "o_id":{"$gte": startOrderId, "$lt": resultDNextOId}})
+		lastOrderListCollection = self.session.order.find({"o_w_id":int(self.w_id), "o_d_id": int(self.d_id), "o_id":{"$gte": startOrderId, "$lt": resultDNextOId}})[0]["o_line"]
 
 		#for every order in last order list collection, extract item id
 		for item in lastOrderListCollection:
+			
 			item_id = item["ol_i_id"]
-			resultCount = self.session.stock_item.aggregate([{$match:{$and:["s_w_id": {"$eq":self.w_id}, "s_i_id":{"$eq":item_id}, "s_quantity":{"$lt":self.stockThreshold}]}}])
-		# get total number of items in S where its stock quantity < T
-			if resultCount < self.stockThreshold:
+			resultCount = self.session.stock_item.aggregate([{"$match":{"$and":[{"s_w_id": {"$eq":int(self.w_id)}}, {"s_i_id":{"$eq":int(item_id)}}, {"s_quantity":{"$lt":int(self.stockThreshold)}}]}}])
+			
+			finalResult = list(resultCount)
+			
+			if finalResult:
 				print "Items below threshold\n"
-				print "Item ID: %d \t 	Total quantity: %d\n"%(item_id,resultCount["s_quantity"])
-		
-
-
-
-
-
+				print "Item ID: %d \t 	Total quantity: %d\n" % (item_id, finalResult[0]["s_quantity"])
