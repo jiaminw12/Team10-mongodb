16d4d1e8e34d9a023e5e5eac5a11969545a21382
Jia Min <jiaminw@ymail.com>
2017-11-08 15:59:40 +0800

    Fixed Order-Status

--- app/OrderStatusTransaction.py
+++ app/OrderStatusTransaction.py
@@ -4,42 +4,42 @@ import pymongo
 
 from time import gmtime, strftime
 
+import pprint
+
 class OrderStatusTransaction(object):
 
 	def __init__(self, session, c_w_id, c_d_id, c_id):
 		self.session = session
-#		self.consistencyLevel = consistencyLevel
 		self.c_w_id = c_w_id
 		self.c_d_id = c_d_id
 		self.c_id = c_id
-#	self.initPreparedStmts()
-	
-#	def initPreparedStmts(self):
-		
 
 	def process(self):
-		
+	
 		# get collection
 		customerCollection = self.session.customer
 		orderCollection = self.session.order
 
 		# get the customer's info
-		select_customer = customerCollection.find_one({"c_w_id":self.c_w_id,"c_d_id":self.c_d_id,"c_id":self.c_id})
+		selected_customer = customerCollection.find_one({"c_w_id":int(self.c_w_id),"c_d_id":int(self.c_d_id),"c_id":int(self.c_id)})
 		
 		# get the last order id
-		select_last_order = orderCollection.find_one({"o_w_id":self.c_w_id,"o_d_id":self.c_d_id,"o_c_id":self.c_id}).sort("o_id",pymongo.DESCENDING)
-		lastOrder = select_last_order["o_id"]
-		#get list of items
-		getItemList = orderCollection.find({"ol_w_id":self.c_w_id, "ol_d_id": self.c_d_id, "ol_o_id":lastOrder},{ol_i_id:1,ol_supply_w_id:1,ol_quantity:1,ol_amount:1,ol_delivery_d:1})
-		#self.select_order = self.session.prepare("SELECT ol_i_id, ol_supply_w_id, ol_quantity, ol_amount, ol_delivery_d from OrderLine where ol_w_id = ? AND ol_d_id = ? AND ol_o_id = ? ALLOW FILTERING")
+		select_last_order = orderCollection.find_one({"o_w_id":int(self.c_w_id),"o_d_id":int(self.c_d_id),"o_c_id":int(self.c_id)}, sort = [("o_id",pymongo.DESCENDING)])
+		
+		if select_last_order:
+		
+			lastOrder = select_last_order["o_id"]
+			
+			getOrderLineList = select_last_order["o_line"]
+			
+			#print customer info
+			print("Customer name: %s %s %s\tBalance: %f"%(selected_customer["c_first"], selected_customer["c_middle"], selected_customer["c_last"],selected_customer["c_balance"]))
 
-
-		#print customer info
-		print("customer name: %s %s %s\tBalance: %f"%(selected_customer["c_first"], selected_customer["c_middle"], selected_customer["c_last"],selected_customer["c_balance"]))
-
-		#print last order id
-		print ("Last Order: %d"%(lastOrder))
-
-		#print item list
-		for item in getItemList:
-			print "OL_i_ID: %d \t OL_SUPPLY_ID: %d \t OL_QUANTITY: %d \t OL_AMOUNT: %f \t OL_DELIVERY_D: %s "%(item["ol_i_id"], item["ol_supply_w_id"], item["ol_quantity"], item["ol_amount"], item["ol_delivery_d"])
+			#print last order id
+			print ("Last Order: %d" % (lastOrder))
+			
+			print ("Entry Date: %s \t Carrier ID: %d \n"%(select_last_order["o_entry_d"], select_last_order["o_carrier_id"]))
+			
+			#print item list
+			for orderLineItem in getOrderLineList:
+				print "OL_i_ID: %d \t OL_SUPPLY_ID: %d \t OL_QUANTITY: %d \t OL_AMOUNT: %f \t OL_DELIVERY_D: %s "%(orderLineItem["ol_i_id"], orderLineItem["ol_supply_w_id"], orderLineItem["ol_quantity"], orderLineItem["ol_amount"], orderLineItem["ol_delivery_d"])
