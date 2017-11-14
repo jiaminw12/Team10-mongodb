8c19880fd6f96d27d961c6cbf81ef30b59cabc7c
Jia Min <jiaminw@ymail.com>
2017-11-15 02:38:08 +0800

    Updated shard.sh

--- shard.sh
+++ shard.sh
@@ -1,22 +1,20 @@
 #!/bin/bash
 
-IFS=$'\n' read -d '' -r -a lines < ~/Team10-mongodb/nodeList.txt
-
 cd /temp/mongodb-linux-x86_64-rhel70-3.4.7/bin
 
 echo -ne "\n Beginning sharding process \n"
-./mongo --host "${lines[2]}" --port 27017 team10 --eval 'sh.enableSharding("team10")'
-./mongo --host "${lines[2]}" --port 27017 team10 --eval 'sh.shardCollection("team10.warehouse_district", {w_id: 1})'
-./mongo --host "${lines[2]}" --port 27017 team10 --eval 'sh.shardCollection("team10.customer", { c_w_id: 1, c_d_id: 1, c_id: 1 })'
-./mongo --host "${lines[2]}" --port 27017 team10 --eval 'sh.shardCollection("team10.order", { o_w_id: 1, o_d_id: 1, o_id: 1 })'
-./mongo --host "${lines[2]}" --port 27017 team10 --eval 'sh.shardCollection("team10.stock_item", { s_w_id: 1, s_i_id: 1})'
+./mongo --host $1 --port 27017 team10 --eval 'sh.enableSharding("team10")'
+./mongo --host $1 --port 27017 team10 --eval 'sh.shardCollection("team10.warehouse_district", {w_id: 1})'
+./mongo --host $1 --port 27017 team10 --eval 'sh.shardCollection("team10.customer", { c_w_id: 1, c_d_id: 1, c_id: 1 })'
+./mongo --host $1 --port 27017 team10 --eval 'sh.shardCollection("team10.order", { o_w_id: 1, o_d_id: 1, o_id: 1 })'
+./mongo --host $1 --port 27017 team10 --eval 'sh.shardCollection("team10.stock_item", { s_w_id: 1, s_i_id: 1})'
 
 echo -ne "\n Starting Balancer \n"
 
-./mongo --host "${lines[2]}" --port 27017 team10 --eval 'sh.startBalancer()'
+./mongo --host $1 --port 27017 team10 --eval 'sh.startBalancer()'
 
 echo -ne "\n Check chunks \n"
 
-./mongo --host "${lines[2]}" --port 27017 team10 --eval 'sh.status()'
+./mongo --host $1 --port 27017 team10 --eval 'sh.status()'
 
 echo -ne "\n End of Sharding \n"
